869988e240418675cd5d5398ec6f4591371a13c8
Brandon Ireland <brandonsireland@gmail.com>
2019-03-04 17:56:03 -0800

    fixed divesite schema

--- server/models/DiveSite.js
+++ server/models/DiveSite.js
@@ -32,59 +32,6 @@ const DiveSiteSchema = new Schema( {
     },
     city_add_2: { 
         type: Schema.Types.ObjectId, 
-        ref: 'CityAdd2Model'
-    },
-    tags: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'TagsModel'
-    },
-    types: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'TypesModel'
-    },
-    access: [String],
-    quality: Number,
-    exp_lvl: [String],
-    bio_lvl: Number,
-    dangers: [String],
-    images: [String]
-});
-
-module.exports = mongoose.model('DiveSiteModel', DiveSiteSchema);
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const DiveSiteSchema = new Schema( {
-    title: String,
-    description: String,
-    geo_lat: Number,
-    geo_long: Number,
-    country_short: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'CountryShortModel'
-    },
-    continent: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'ContinentModel'
-    },
-    country: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'CountryModel'
-    },
-    region: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'RegionModel'
-    },
-    city: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'CityModel'
-    },
-    city_add: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'CityAddModel'
-    },
-    city_add_2: { 
-        type: Schema.Types.ObjectId, 
         ref: 'CityAddTwoModel'
     },
     tags: [{ 
